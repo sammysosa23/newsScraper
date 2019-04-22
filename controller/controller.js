40acbfde2b6daa60cbaf1dd22f0b2f04abcd976d
Sam Sosa <sam.terrazas@yahoo.com>
2019-04-22 18:34:14 -0400

    updated controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -157,38 +157,4 @@ router.post("/comment/:id", function(req, res) {
   });
 });
 
-// router.post("/comment/:id", function(req, res) {
-//   var user = req.body.name;
-//   var content = req.body.comment;
-//   var articleId = req.params.id;
-
-//   var commentObj = {
-//     name: user,
-//     body: content
-//   };
-
-//   var newComment = new Comment(commentObj);
-
-//   newComment.save(function(err, doc) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(doc._id);
-//       console.log(articleId);
-
-//       Article.findOneAndUpdate(
-//         { _id: req.params.id },
-//         { $push: { comment: doc._id } },
-//         { new: true }
-//       ).exec(function(err, doc) {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           res.redirect("/readArticle/" + articleId);
-//         }
-//       });
-//     }
-//   });
-// });
-
 module.exports = router;
